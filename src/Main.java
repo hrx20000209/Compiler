db38ed63e8d5e392f5af858f386d9100a2e93c8b
黄润希 <417336070@qq.com>
2021-10-25 08:34:44 +0800

    judge

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         String a = input.next();
         String b = input.next();
         System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
