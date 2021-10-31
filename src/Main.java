a274c6b09468a0e7fa5778b5f3b42c3357ade7fc
黄润希 <417336070@qq.com>
2021-10-31 10:00:01 +0800

    push

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String a = input.next();
-        String b = input.next();
-        System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
+        while(input.hasNext()) {
+            String str = input.next();
+        }
     }
 }
